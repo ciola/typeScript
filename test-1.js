9275122b83d6a2d100f27fe8f906e1a08160a1d8
ciola <loicgeeraerts@MacBook-Air-de-loic.local>
2016-12-24 02:06:22 -0500

    message de validation

---
+++ test-1.js
@@ -0,0 +1,3 @@
+var ru = { firstName: "Jane", lastName: "User" };
+document.body.innerHTML = ru.firstName;
+//document.body.innerHTML = "lolo"; 
---
+++ test-1.ts
@@ -0,0 +1,6 @@
+
+
+var ru = { firstName: "Jane", lastName: "User" };
+
+document.body.innerHTML = ru.firstName;
+//document.body.innerHTML = "lolo";
